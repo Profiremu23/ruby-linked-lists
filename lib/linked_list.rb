93ca2fe2fe5fefacb27a7113c680e06e6429bb84
Profiremu23 <profirdani23@gmail.com>
2024-07-29 16:21:20 +0200

    Added basic comments and frozen string literal config

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,45 @@
-class LinkedList
+# frozen_string_literal: true
 
+# To represent the missing linked list class for Ruby
+class LinkedList
+  def apprend(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
 end
 
+# The building block for any linked list
 class Node
+  include LinkedList
 
+  def value
+  end
+
+  def next_node
+  end
 end
