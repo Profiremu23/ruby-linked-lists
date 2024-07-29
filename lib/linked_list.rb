40668f8424df84aa7b30c58334acc5997150dafe
Profiremu23 <profirdani23@gmail.com>
2024-07-29 17:57:04 +0200

    Rewritten Node class and added some test examples for the upcoming LinkedList implementations

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -34,12 +34,24 @@ class LinkedList
 end
 
 # The building block for any linked list
-class Node
-  include LinkedList
+Node = Struct.new(:value, :next_node)
 
-  def value
-  end
+# For debugging purposes
+node1 = Node.new # To test Node.value's storage capability
+p node1.value
+node1.value = 'Hello'
+p node1.value
 
-  def next_node
-  end
-end
+node2 = Node.new("It's me") # For testing out LinkedList connectivity
+node3 = Node.new('Profiremu23')
+node4 = Node.new("I'm in Spain without the S")
+
+node1.next_node = node2
+node2.next_node = node3
+node3.next_node = node4
+
+p node1.next_node.value
+p node2.next_node.value
+p node3.next_node.value
+p node4.next_node
+p node1.next_node.next_node.next_node.value
